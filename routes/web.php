a64f7e84dedde3c728cf1eef308bbc9c63034633
dmjrmh <gban1mj27@gmail.com>
2025-09-04 15:17:15 +0700

    added middleware for sales and manager only

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::middleware('role:manager')->group(function () {
+      Route::get('/reports', fn() => 'Laporan semua sales');
+    });
+
+    Route::middleware('role:sales')->group(function () {
+      Route::get('/leads', fn() => 'Daftar Leads');
+    });
 });
 
 require __DIR__.'/auth.php';
