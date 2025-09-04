0b370e3f73270cdaa8a948438cf6f4913bc71ece
dmjrmh <gban1mj27@gmail.com>
2025-09-05 03:59:29 +0700

    added migration products table

---
+++ database/migrations/2025_09_04_165332_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('products', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('sku')->unique();
+      $table->string('unit', 20)->default('pcs');
+      $table->decimal('cost_price', 15, 2);
+      $table->decimal('margin_percent', 8, 2)->default(0);
+      $table->decimal('sell_price', 15, 2);
+      $table->text('description')->nullable();
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('products');
+  }
+};
